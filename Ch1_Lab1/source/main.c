03c20adb245e42ea8ecaed2a3098a4191b742450
a2495948 <a2495948@gmail.com>
2019-10-24 19:52:14 +0800

    first

---
+++ Ch1_Lab1/source/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{ 
+	printf("Welcome to C!\n");
+	printf("my student id is:108360246\n");
+	printf("My name is Jun Fung\n");
+	system("pause");
+	return 0; 
+
+}
